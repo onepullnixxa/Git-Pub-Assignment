e6b27ea049acbd8b99f145b1f8c315faec8835ac
onepullnixxa <briseno.chris@gmail.com>
2021-09-22 23:20:50 -0400

    renders food and drinks

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 const drinks = require('./models/drinks.js');
+const food = require('./models/food.js');
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Gitpub App");
@@ -12,11 +13,21 @@ app.get('/drinks/', (req, res) => {
     res.render("index.ejs", {allDrinks:drinks});
 });
 
+app.get('/food/', (req, res) => {
+    res.render('food_index.ejs', {allFood:food});
+});
+
 app.get('/drinks/:id', (req, res) => {
     res.render("show.ejs", {
         drink: drinks[req.params.id]
     });
-})
+});
+
+app.get('/food/:id', (req, res) => {
+    res.render("food_show.ejs", {
+        food: food[req.params.id]
+    });
+});
 
 app.listen(port, () => {
     console.log("listening on port: ", port);
